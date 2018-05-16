58b63a3aa82e289bfc8bac0450247a9e0286b205
zehnpaard <zehnpaard@googlemail.com>
2018-05-17 08:58:13 +0900

    Add test cases for showing TmVar

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,14 @@
 import Test.HUnit
 
+import Types
+
 main :: IO ()
 main = do
-  runTestTT $ TestList []
+  runTestTT $ TestList [showTmVarTest]
   return ()
+
+showTmVarTest :: Test
+showTmVarTest = TestList [
+  "Test 1:" ~: (show $ TmVar "test") ~?= "test",
+  "Test 2:" ~: (show $ TmVar "a") ~?= "a",
+  "Test 3:" ~: (show $ TmVar "") ~?= ""]
