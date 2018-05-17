c7a501dfe9bd2a26801ef8a014f28284d6298bcc
zehnpaard <zehnpaard@googlemail.com>
2018-05-17 09:12:06 +0900

    Add parser for variable term with tests

--- src/Types.hs
+++ src/Types.hs
@@ -1,6 +1,7 @@
 module Types where
 
 data Term = TmVar String
+  deriving Eq
 
 instance Show Term where show = showTerm
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,11 @@
 import Test.HUnit
 
 import Types
+import Parser
 
 main :: IO ()
 main = do
-  runTestTT $ TestList [showTmVarTest]
+  runTestTT $ TestList [showTmVarTest, parseTmVarTest]
   return ()
 
 showTmVarTest :: Test
@@ -12,3 +13,10 @@ showTmVarTest = TestList [
   "Test 1:" ~: (show $ TmVar "test") ~?= "test",
   "Test 2:" ~: (show $ TmVar "a") ~?= "a",
   "Test 3:" ~: (show $ TmVar "") ~?= ""]
+
+parseTmVarTest :: Test
+parseTmVarTest = TestList [
+  "Test 1:" ~: (readExpr "x") ~?= (TmVar "x"),
+  "Test 2:" ~: (readExpr "abc") ~?= (TmVar "abc"),
+  "Test 3:" ~: (readExpr "z1") ~?= (TmVar "z1")
+  ]
