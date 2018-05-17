a8841bee6f0d341391e712dfd93b656a65b57744
zehnpaard <zehnpaard@googlemail.com>
2018-05-17 18:26:44 +0900

    Add lambda abstraction type definition

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,18 +5,30 @@ import Parser
 
 main :: IO ()
 main = do
-  runTestTT $ TestList [showTmVarTest, parseTmVarTest]
+  runTestTT $ TestList [
+    showTmVarTest, 
+    parseTmVarTest,
+    showTmAbsTest
+    ]
   return ()
 
 showTmVarTest :: Test
 showTmVarTest = TestList [
   "Test 1:" ~: (show $ TmVar "test") ~?= "test",
   "Test 2:" ~: (show $ TmVar "a") ~?= "a",
-  "Test 3:" ~: (show $ TmVar "") ~?= ""]
+  "Test 3:" ~: (show $ TmVar "") ~?= ""
+  ]
 
 parseTmVarTest :: Test
 parseTmVarTest = TestList [
   "Test 1:" ~: (readExpr "x") ~?= (TmVar "x"),
   "Test 2:" ~: (readExpr "abc") ~?= (TmVar "abc"),
   "Test 3:" ~: (readExpr "z1") ~?= (TmVar "z1")
+  ]
+
+showTmAbsTest :: Test
+showTmAbsTest = TestList [
+  "Test 1:" ~: (show $ TmAbs "x" $ TmVar "x") ~?= "λx.x",
+  "Test 2:" ~: (show $ TmAbs "x" $ TmVar "y") ~?= "λx.y",
+  "Test 3:" ~: (show $ TmAbs "x" $ TmAbs "y" $ TmVar "z") ~?= "λx.λy.z"
   ]
